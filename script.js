cf53698c81839844c310644ef33b823075b20f3c
Nikita <105992775+GiShame@users.noreply.github.com>
2022-05-22 21:14:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -59,30 +59,32 @@
 
 // Introduction to Objects
 
-const myCountry = {
-   country: 'Belarus',
-   capital: 'Minsk',
-   language: 'russian',
-   population: 10,
-   neighbours: ['Russia', 'Poland', 'Lithuania'],
-   description: function () {
-   console.log(`There is ${this.country} with a capital named ${this.capital}`)
-   },
-   checkIsland: function() {
-      this.isIsland = this.neighbours.length === 0 ? true : false
-   }
-}
-myCountry.description()
-myCountry.checkIsland();
-console.log(myCountry);
+// const myCountry = {
+//    country: 'Belarus',
+//    capital: 'Minsk',
+//    language: 'russian',
+//    population: 10,
+//    neighbours: ['Russia', 'Poland', 'Lithuania'],
+//    description: function () {
+//    console.log(`There is ${this.country} with a capital named ${this.capital}`)
+//    },
+//    checkIsland: function() {
+//       this.isIsland = this.neighbours.length === 0 ? true : false
+//    }
+// }
+// myCountry.description()
+// myCountry.checkIsland();
+// console.log(myCountry);
 
-console.log(`${myCountry.country} has ${myCountry.population - 2} ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}`)
+// console.log(`${myCountry.country} has ${myCountry.population - 2} ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}`)
 
-myCountry['population'] += 2;
-console.log(myCountry)
+// myCountry['population'] += 2;
+// console.log(myCountry)
 
 // introducing to loop
 
 // for(voter = 1; voter <= 50; voter++) {
 //    console.log(`Voter number ${voter}  is currently voting`)
 // }
+
+const population = [10, 80, 100, 70]
