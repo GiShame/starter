4d7fa26195d70866bec44d964ff4b579e3f2fce6
Nikita <105992775+GiShame@users.noreply.github.com>
2023-07-06 02:48:18 +0300

    This, func
    regular vs arrow func., keyword this

--- script.js
+++ script.js
@@ -1,156 +1,233 @@
-//challenge (objects methods)
+'use strict';
 
-// const Mark = {
-//     name: 'Mark',
-//     mass: 78,
-//     height: 1.69,
+///////////////////////////////////////
+// Scoping in Practice
 
-//     calcBMI: function () {
-//         this.bmi = this.mass / (this.height ** 2)
-//         return this.bmi
-//     }
-// }
+/*
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
 
-// const John = {
-//     name: 'John',
-//     mass: 92,
-//     height: 1.95,
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
 
-//     calcBMI: function () {
-//         this.bmi = this.mass / (this.height ** 2)
-//         return this.bmi
-//     }
-// }
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      // Creating NEW variable with same name as outer scope's variable
+      const firstName = 'Steven';
 
-// Mark.calcBMI();
-// John.calcBMI();
-// if (John.bmi > Mark.bmi) {
-//     console.log (`${John.name}'s BMI ${John.bmi} is higher than ${Mark.name}'s BMI ${Mark.bmi}`)
-// } else {
-//     console.log (`${Mark.name}'s BMI ${Mark.bmi} is higher than ${John.name}'s BMI ${John.bmi}`)
-// }
+      // Reasssigning outer scope's variable
+      output = 'NEW OUTPUT!';
 
-// Introduction to Arrays
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
 
-// const population = [10, 80, 100, 70];
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // console.log(str);
+    console.log(millenial);
+    // console.log(add(2, 3));
+    console.log(output);
+  }
+  printAge();
 
-// function percentageOfWorld1(population) {
-//    return (population / 7900) * 100;
-// }
-// const percentages = [
-//  percentageOfWorld1(population[0]),
-//  percentageOfWorld1(population[1]),
-//  percentageOfWorld1(population[2]),
-//  percentageOfWorld1(population[3])
-// ];
-// console.log(percentages);
+  return age;
+}
 
-// Array methods
-
-// let neighbours = ['Russia', 'Poland', 'Lithuania']
-
-// neighbours.push('Utopia')
-// console.log(neighbours)
-// neighbours.pop();
-// console.log(neighbours)
-// neighbours.includes('Germany') ? console.log('Cobiray shmotki') : console.log('Probably not a central European country')
-// neighbours[neighbours.indexOf('Polad')] = 'Republic of Poland'
-// console.log(neighbours)
-
-// Introduction to Objects
-
-// const myCountry = {
-//    country: 'Belarus',
-//    capital: 'Minsk',
-//    language: 'russian',
-//    population: 10,
-//    neighbours: ['Russia', 'Poland', 'Lithuania'],
-//    description: function () {
-//    console.log(`There is ${this.country} with a capital named ${this.capital}`)
-//    },
-//    checkIsland: function() {
-//       this.isIsland = this.neighbours.length === 0 ? true : false
-//    }
-// }
-// myCountry.description()
-// myCountry.checkIsland();
-// console.log(myCountry);
-
-// console.log(`${myCountry.country} has ${myCountry.population - 2} ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}`)
-
-// myCountry['population'] += 2;
-// console.log(myCountry)
-
-// introducing to loop
-
-// for(voter = 1; voter <= 50; voter++) {
-//    console.log(`Voter number ${voter}  is currently voting`)
-// }
-
-// const population1 = [10, 80, 100, 70];
-// const percentages2 = [];
-// function percentageOfWorld1(population) {
-//   return (population / 7900) * 100;
-// }
-// for (let i = 0; i < population1.length; i++) {
-//   percentages2.push(percentageOfWorld1(population1[i]));
-// }
-
-// console.log(percentages2);
-
-// const listOfNeighbours = [['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden', 'Russia']]
-// for (let i = 0; i < listOfNeighbours.length; i++) {
-//    listOfNeighbours[i]
-
-//    for (let c = 0; c < listOfNeighbours[i].length; c++) {
-//       listOfNeighbours[c]
-//       console.log(listOfNeighbours[i][c])
-//    }
-// }
-
-// const populations = [10, 80, 100, 70]
-// const percentages3 = []
-// function percentageOfWorld1(population) {
-//   return (population / 7900) * 100;
-// }
-// let i = 0;
-// while (i < populations.length) {
-//    percentages3.push(percentageOfWorld1(populations[i]))
-//    i++
-// }
-// console.log (percentages3)
+const firstName = 'Jonas';
+calcAge(1991);
+// console.log(age);
+// printAge();
 
 
-// LAST CHALLENGE FOUNDAMENTALS PART 2
+///////////////////////////////////////
+// Hoisting and TDZ in Practice
 
-// const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52]
-// const tips = []
-// const totalValues = []
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
 
-// const calcTip = function (bills) {
-//    return bills >= 50 && bills <=300 ? bills * 0.15 : bills * 0.2;
-// }
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
 
-// for (let i = 0; i < bills.length; i++) {
-//    tips.push(calcTip(bills[i]))
-//    totalValues.push(bills[i] + tips[i])
-// }
-// console.log (tips)
-// console.log (totalValues)
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
 
-// //Bonus task
+function addDecl(a, b) {
+  return a + b;
+}
 
-// const test = function (arr) {
-// let sum = 0
-// for (let b = 0; b < arr.length; b++) {
-//    sum += arr[b];
-// }
-// return sum
-// }
-// console.log(test(bills))
+const addExpr = function (a, b) {
+  return a + b;
+};
 
-// const a = document.querySelector('.a')
-// a.addEventListener('click',() => {
-//    console.log(`Hello world!`)
-// })
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(undefined);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
+
+
+///////////////////////////////////////
+// The this Keyword in Practice
+console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAgeArrow(1980);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+f();
+
+
+///////////////////////////////////////
+// Regular Functions vs. Arrow Functions
+// var firstName = 'Matilda';
+
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    // console.log(this);
+    console.log(2037 - this.year);
+
+    // Solution 1
+    // const self = this; // self or that
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    // };
+
+    // Solution 2
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
+  },
+
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
+  },
+};
+jonas.greet();
+jonas.calcAge();
+
+// arguments keyword
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 8);
+
+
+///////////////////////////////////////
+// Objects vs. primitives
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+  name: 'Jonas',
+  age: 30,
+};
+const friend = me;
+friend.age = 27;
+console.log('Friend:', friend);
+console.log('Me', me);
+
+
+///////////////////////////////////////
+// Primitives vs. Objects in Practice
+
+// Primitive types
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldLastName);
+
+// Reference types
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+console.log('Before marriage:', jessica);
+console.log('After marriage: ', marriedJessica);
+// marriedJessica = {};
+
+// Copying objects
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log('Before marriage:', jessica2);
+console.log('After marriage: ', jessicaCopy);
+*/
 
